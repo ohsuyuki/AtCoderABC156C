751e67b5ac506feb4d1439fafc6e9a54b2999bc8
ohsuyuki <papu8osu@gmail.com>
2020-02-24 13:41:15 +0900

    Initial Commit

---
+++ AtCoderC20200224/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AtCoderC20200224
+//
+//  Created by yuuki oosu on 2020/02/24.
+//  Copyright © 2020 yuuki oosu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
